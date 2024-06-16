d24b6924597f7e7caedac0852dfeb20f49c1b786
kashif <kashif_saleem_23@hotmail.com>
2024-06-16 13:06:31 +0500

    Refac: Fixed the cards

--- frontend/src/app/chat/page.js
+++ frontend/src/app/chat/page.js
@@ -19,79 +19,79 @@ const drawerWidth = 300;
 export default function PermanentDrawerLeft() {
   return (
     <>
-      <Container sx={{ height: "100%", mx: 1, pl: 0 }}>
-        <Box
+      {/* <Container sx={{ height: "100%", width: "100%", mx: 1, pl: 0 }} > */}
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "10% 30% 1fr",
+          gridTemplateRows: "10% 30% 1fr",
+          gap: 3,
+          height: "100%",
+          p: 3,
+        }}
+      >
+        {/* The side bar */}
+        <Paper
+          elevation={3}
           sx={{
-            display: "grid",
-            gridTemplateColumns: "10% 30% 60%",
-            gridTemplateRows: "10% 30% 1fr",
-            gap: 3,
-            height: "100%",
+            gridColumn: "1",
+            gridRow: "1 / 4",
+            borderRadius: "16px",
             p: 2,
           }}
         >
-          {/* The side bar */}
-          <Paper
-            elevation={3}
-            sx={{
-              gridColumn: "1",
-              gridRow: "1 / 4",
-              borderRadius: "16px",
-              p: 2,
-            }}
-          >
-            hello
-          </Paper>
-          {/* Search Bar */}
-          <Paper
-            elevation={3}
-            sx={{
-              gridColumn: "2",
-              gridRow: "1 / 2",
-              borderRadius: "16px",
-              p: 2,
-            }}
-          >
-            hello
-          </Paper>
-          {/* Groups */}
-          <Paper
-            elevation={3}
-            sx={{
-              gridColumn: "2",
-              gridRow: "2/ 3",
-              borderRadius: "16px",
-              p: 2,
-            }}
-          >
-            hello
-          </Paper>
-          {/* People */}
-          <Paper
-            elevation={3}
-            sx={{
-              gridColumn: "2",
-              gridRow: "3/ 4",
-              borderRadius: "16px",
-              p: 2,
-            }}
-          >
-            hello
-          </Paper>
-          {/* Chat Area */}
-          <Paper
-            elevation={3}
-            sx={{
-              gridColumn: "3",
-              gridRow: "1/ 4",
-              borderRadius: "16px",
-              p: 2,
-            }}
-          >
-            hello
-          </Paper>
-        </Box>
-      </Container>
+          hello
+        </Paper>
+        {/* Search Bar */}
+        <Paper
+          elevation={3}
+          sx={{
+            gridColumn: "2",
+            gridRow: "1 / 2",
+            borderRadius: "16px",
+            p: 2,
+          }}
+        >
+          hello
+        </Paper>
+        {/* Groups */}
+        <Paper
+          elevation={3}
+          sx={{
+            gridColumn: "2",
+            gridRow: "2/ 3",
+            borderRadius: "16px",
+            p: 2,
+          }}
+        >
+          hello
+        </Paper>
+        {/* People */}
+        <Paper
+          elevation={3}
+          sx={{
+            gridColumn: "2",
+            gridRow: "3/ 4",
+            borderRadius: "16px",
+            p: 2,
+          }}
+        >
+          hello
+        </Paper>
+        {/* Chat Area */}
+        <Paper
+          elevation={3}
+          sx={{
+            gridColumn: "3",
+            gridRow: "1/ 4",
+            borderRadius: "16px",
+            p: 2,
+          }}
+        >
+          hello
+        </Paper>
+      </Box>
+      {/* </Container> */}
     </>
   );
 }
