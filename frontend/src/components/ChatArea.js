83bbfc9ed628070b231c8d2e617fb73c700e1447
kashif <kashif_saleem_23@hotmail.com>
2024-06-25 22:53:27 +0500

    Feat: Updated message area component

--- frontend/src/components/ChatArea.js
+++ frontend/src/components/ChatArea.js
@@ -1,9 +1,13 @@
 import ChatHeader from "@/components/chatAreaComponents/ChatHeader.js";
+import MessagingArea from "@/components/chatAreaComponents/MessagingArea.js";
+import TextEditor from "@/components/chatAreaComponents/TextEditor.js";
 
 export default function ChatArea() {
   return (
     <>
       <ChatHeader />
+      <MessagingArea />
+      <TextEditor />
     </>
   );
 }
---
+++ frontend/src/components/chatAreaComponents/TextEditor.js
@@ -0,0 +1,16 @@
+import Box from "@mui/material/Box";
+
+export default function TextEditor() {
+  return (
+    <Box
+      sx={{
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "10px",
+        height: "100px",
+      }}
+    >
+      Hello from text Editor
+    </Box>
+  );
+}
