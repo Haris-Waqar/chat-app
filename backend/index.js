2a379116d03dc7580a38df361c5d0c48312a7099
Haris Waqar <haris_waqar@192.168.1.8>
2024-06-28 20:07:11 +0500

    feat. implement bi-directional chat using socket.io

--- backend/index.js
+++ backend/index.js
@@ -32,22 +32,58 @@ app.get("/", (req, res) => {
 
 const server = createServer(app);
 const io = new Server(server);
+let onlineUsers = [];
 
 io.on("connection", (socket) => {
   socket.removeAllListeners();
-  console.log("a user connected");
+  console.log(`User connected with Socket ID ${socket.id}`);
 
-  socket.on("sendMessage", (msgObj) => {
-    console.log("message:", msgObj.name + msgObj.message);
-    socket.emit("message", {
-      sender: "bot",
-      message: "Thank you. Please wait for a moment",
-      time: new Date().toLocaleTimeString(),
+  // listen to a connection
+  socket.on("addNewUser", (userId) => {
+    console.log("UserId is", userId);
+    // first check we don't have the user already
+    const existingUser = onlineUsers.find((user) => user.userId === userId);
+    if (existingUser) {
+      console.log("User already exists");
+      return;
+    }
+
+    // Add the new user to the list of online users
+    console.log(`Adding user with ID: ${userId}`);
+    onlineUsers.push({
+      userId,
+      socketId: socket.id,
     });
+    console.log("Online Users", onlineUsers);
+  });
+
+  // listen to a private message
+  socket.on("sendMessage", (msgObj) => {
+    const user = onlineUsers.find((user) => user.userId === msgObj.receiverId);
+    if (user) {
+      console.log(`Sending message to user with ID: ${msgObj.receiverId}`);
+      io.to(user.socketId).emit("getMessage", msgObj);
+    }
+    // socket.emit("message", {
+    //   sender: "bot",
+    //   message: "Thank you. Please wait for a moment",
+    //   time: new Date().toLocaleTimeString(),
+    // });
+    // let newMessage = msgObj.message;
+    // console.log(socket.id);
+    // socket.to(msgObj.to).emit("private message", {
+    //   newMessage,
+    //   from: socket.id,
+    // });
   });
 
   socket.on("disconnect", () => {
     console.log(`Socket ${socket.id} disconnected`);
+    const index = onlineUsers.findIndex((user) => user.socketId === socket.id);
+    if (index > -1) {
+      onlineUsers.splice(index, 1);
+    }
+    console.log("Online Users", onlineUsers);
   });
 });
 
