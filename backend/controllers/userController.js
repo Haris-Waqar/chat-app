afb630258594ca451bbd09d006c07ed7b9457cde
kashif <kashif_saleem_23@hotmail.com>
2024-06-28 23:52:17 +0500

    Feat: made the get all users api

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/user");
+
+// Fetch All Users From The Database
+
+const getUsers = async (req, res) => {
+  console.log("Fetching Users");
+  try {
+    const users = await User.find();
+    return res.status(200).json({ users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return res.status(500).json({ message: "Error Fetching Users" });
+  }
+};
+
+module.exports = { getUsers };
